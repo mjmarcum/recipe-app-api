9de77a058269fa9b652beba17c8b046fe64b35df
Michael <michael.j.marcum@gmail.com>
2025-03-26 22:47:53 -1000

    fixed linting

--- app/app/test.py
+++ app/app/test.py
@@ -11,13 +11,12 @@ class CalcTests(SimpleTestCase):
 
   def test_add_sumbers(self):
     # test adding numbers together
-    res = calc.add(5,6)
+    res = calc.add(5, 6)
 
-    self.assertEqual(res,11)
+    self.assertEqual(res, 11)
 
   def test_subtract_numbers(self):
     # Test subtracting numbers
-    res = calc.subtract(10,15)
+    res = calc.subtract(10, 15)
 
-    self.assertEqual(res,5)
-
+    self.assertEqual(res, 5)
